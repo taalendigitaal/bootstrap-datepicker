70cc09feb7a4a43c3b9bdca494d27c7d50a76a30
Otte Verbeek <otte@taalendigitaal.nl>
2014-05-08 10:59:19 +0200

    Added nl-FR locale

---
+++ js/locales/bootstrap-datepicker.fr-NL.js
@@ -0,0 +1,15 @@
+/**
+ * Frisian translation for bootstrap-datepicker
+ * Taal&Digitaal <info@taalendigitaal.nl>
+ */
+;(function($){
+	$.fn.datepicker.dates['fr-NL'] = {
+		days: ["Snein", "Moandei", "Tiisdei", "Woansdei", "Tongersdei", "Freed", "Sneon", "Snein"],
+		daysShort: ["Sne", "Moa", "Tii", "Woa", "Ton", "Fre", "Sat", "Sne"],
+		daysMin: ["Sn", "Mo", "Ti", "Wo", "To", "Fr", "Sa", "Sn"],
+		months: ["Jannewaris", "Febrewaris", "Maart", "April", "Maaie", "Juny", "July", "Augustus", "Septimber", "Oktober", "Novimber", "Desimber"],
+		monthsShort: ["Jan", "Feb", "Mrt", "Apr", "Maa", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Des"],
+		today: "Hjoed",
+		clear: 'Leechmeitsje'
+	};
+}(jQuery));
